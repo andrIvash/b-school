0f42a67356bce5b50c87229b2fee60b40e5b7c54
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-09-02 16:41:38 +0300

    add autorise and helper methods

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,23 +1,57 @@
 // @flow
 import React, { Component } from 'react';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
-import helpers from '../../helpers';
+import { API } from '../../helpers/api';
+import { Cookie } from '../../helpers/cookie';
+import { Auth } from '../../helpers/auth';
 import type { Film, PosterData, View } from '../../flow-types.js';
 
 type State = {};
 
+const api = new API();
+
 class App extends Component<{}, State> {
 
-  state = {}
+  state = {
+    isLoading: false,
+    user: null,
+  }
 
-  componentDidMount() {}
+  componentDidMount() {
+    console.log('Running App version ! ' + window.env.API );
+    api.init();
+    if (void 0 !== Cookie.get('token')) {
+      api.token = Cookie.get('token');
+      this.getUserInfo();
+    } else if (document.location.search.indexOf('auth') === -1) {
+      Auth.auth();
+    }
+  }
+
+  getUserInfo() {
+    this.setState({ isLoading: true });
+    api.getUserInfo().then((res) => {
+      this.setState({ isLoading: false });
+      console.log('res__', res);
+      const roles = api.getData().user.roles;
+      if (this.isEdu(roles) || this.isParent(roles)) {
+        console.log('Активность возможна только для преподавателей и родителей!');
+      } else {
+        console.log('wellcome !');
+      }
+    });
+  }
+
+  isEdu(roles) { roles.find(role => role === 'EduStaff'); }
+
+  isParent(roles) { roles.find(role => role === 'EduParent'); }
 
   render() {
     return (
       <div className='wrapper'>
         <ErrorBoundary>
           <header>header</header>
-          <main>main</main>
+          <main className='main'></main>
           <footer>footer</footer>
         </ErrorBoundary>
       </div>
