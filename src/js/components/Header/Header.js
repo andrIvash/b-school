35fb222d39d1eef429e88c0814c69e1c0349cbf6
Andriy Ivashchenko <andriy.ivashchenko@thomsonreuters.com>
2018-09-04 18:59:06 +0300

    add base styles

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -78,10 +78,38 @@ const Header = (props: Props) => (
     <div className='narrow'>
       <nav className='narrowLinks animated'>
         <div className='narrowLinks__wrapper'>
-          <a className='menu__link' data-screen='main' href='/' onClick={(evt) => props.burgerToggle(evt)}>Главная</a>
-          <a className='menu__link' data-screen='consultant' href='/' onClick={(evt) => props.burgerToggle(evt)}>Интерактивный консультант</a>
-          <a className='menu__link' data-screen='bullying' href='/' onClick={(evt) => props.burgerToggle(evt)}>Инфографика о буллинге</a>
-          <a className='menu__link' data-screen='lice' href='/' onClick={(evt) => props.burgerToggle(evt)}>Как защитить себя и ребенка от вшей</a>
+          <a
+            className='menu__link'
+            data-screen='main'
+            href='/'
+            onClick={(evt) => props.burgerToggle(evt)}
+            >
+            Главная
+          </a>
+          <a
+            className='menu__link'
+            data-screen='consultant'
+            href='/'
+            onClick={(evt) => props.burgerToggle(evt)}
+            >
+            Интерактивный консультант
+          </a>
+          <a
+            className='menu__link'
+            data-screen='bullying'
+            href='/'
+            onClick={(evt) => props.burgerToggle(evt)}
+            >
+            Инфографика о буллинге
+          </a>
+          <a
+            className='menu__link'
+            data-screen='lice'
+            href='/'
+            onClick={(evt) => props.burgerToggle(evt)}
+            >
+            Как защитить себя и ребенка от вшей
+          </a>
         </div>
       </nav>
     </div>
--- src/js/components/Main/Main.js
+++ src/js/components/Main/Main.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react';
-import Chat from '../Chat/Chat';
 
 type Props = {
   selectScreen: (evt: EventTarget) => void,
@@ -9,42 +8,121 @@ type Props = {
 
 const Main = (props: Props) => (
   <div className='main'>
-    main
-    <a
-      data-screen='lice'
-      href='/'
-      onClick={(evt) => props.selectScreen(evt)}
-      >
-      lice
-    </a>
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    sdfsdfsdfdsfsdfsdf
-    sdfsdfsdfsdfsdfsdf
-    sdfsdfsdfsdfsfsdfs
-    <Chat />
+    <div className='container'>
+      <h1 className='page-title'>
+        title
+      </h1>
+      <p className='page-title__text'>title text</p>
+      <div className='content'>
+        <article className='article'>
+          <section className='section'>
+            <h4 className='section__title'>Heading</h4>
+            <div className='section__subtitle'>subtitle</div>
+            <p className='section__text'>text</p>
+            <a
+              className='section__link'
+              data-screen='main'
+              href='/'
+              onClick={(evt) => props.selectScreen(evt)}
+              title='Лого'
+              >
+              link
+            </a>
+          </section>
+          <section className='section'>
+            <picture>
+              <img
+                alt='Паранит'
+                data-screen='main'
+                src={`${window.env.apiHost}/images/LOGO.png`}
+              />
+            </picture>
+          </section>
+        </article>
+        <ul className='sectionList'>
+          <li className='sectionList__elem'>
+            list text
+          </li>
+          <li className='sectionList__elem'>
+            list text
+          </li>
+          <li className='sectionList__elem'>
+            list text
+          </li>
+          <li className='sectionList__elem'>
+            list text
+          </li>
+        </ul>
+
+        <ul className='sectionList sectionList--foto'>
+          <li className='sectionList__elem'>
+            <picture>
+              <img
+                alt='Паранит'
+                src={`${window.env.apiHost}/images/icon1.png`}
+              />
+            </picture>
+            <p>
+              text
+            </p>
+          </li>
+          <li className='sectionList__elem'>
+            <picture>
+              <img
+                alt='Паранит'
+                src={`${window.env.apiHost}/images/icon1.png`}
+              />
+            </picture>
+            <p>
+              text
+            </p>
+          </li>
+          <li className='sectionList__elem'>
+            <picture>
+              <img
+                alt='Паранит'
+                src={`${window.env.apiHost}/images/icon1.png`}
+              />
+            </picture>
+            <p>
+              text text text text text text text text text text text text text text text text text text 
+            </p>
+          </li>
+          <li className='sectionList__elem'>
+            <picture>
+              <img
+                alt='Паранит'
+                src={`${window.env.apiHost}/images/icon1.png`}
+              />
+            </picture>
+            <p>
+              text
+            </p>
+          </li>
+          
+        </ul>
+
+        <section className='sectionFoto'>
+          <picture>
+            <img
+              alt='Паранит'
+              src={`${window.env.apiHost}/images/icon1.png`}
+            />
+          </picture>
+          <ul className='sectionFoto__sublist'>
+            <li className='sectionFoto__elem'>
+              list text
+            </li>
+            <li className='sectionFoto__elem'>
+              list text
+            </li>
+            <li className='sectionFoto__elem'>
+              list text
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
   </div>
 );
 
